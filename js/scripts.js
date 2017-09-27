47d6d97baf5e4f5df4a60f59ad4943cc46519378
aquarius97 <15369492903@163.com>
2017-09-27 20:58:27 +0800

    dev上添加内容

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
+/*
+ * @Author: 白若年 
+ * @Date: 2017-09-27 20:54:46 
+ * @Last Modified by:   白若年_aquarius 
+ * @Last Modified time: 2017-09-27 20:54:46 
+ */
 ;
 (function($) {
+    var con = new Swiper('.con');
     $('.header p').on('click', function() {
+        var ind = $(this).index();
         $(this).addClass('bg').siblings().removeClass('bg');
+        con.slideTo(ind);
+        if ($(this).hasClass('bg')) {
+            con.slideTo(ind);
+        }
     })
 })(jQuery)
